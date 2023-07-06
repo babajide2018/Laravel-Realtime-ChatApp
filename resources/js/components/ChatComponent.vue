2a354f556db470cadbcffc9b6b18652ccf149a98
Ojo Babajide Joshua <75325075+babajide2018@users.noreply.github.com>
2023-07-06 20:47:47 +0100

    Initial Commit

---
+++ resources/js/components/ChatComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="chat-container">
+        <div class="messages">
+            <div v-for="message in messages" :key="message.id" class="message">
+                <strong>{{ message.user }}</strong>: {{ message.content }}
+            </div>
+        </div>
+        <div class="input-area">
+            <input v-model="newMessage" @keyup.enter="sendMessage" type="text" placeholder="Type your message here...">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: [],
+            newMessage: '',
+        };
+    },
+    methods: {
+        sendMessage() {
+            // Logic to send a new message will go here
+        },
+    },
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<div id="app">
+    <chat-component></chat-component>
+</div>
